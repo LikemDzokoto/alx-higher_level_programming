1bf95dc562531f40086d549375d1be729d3aed49
LikemDzokoto <likemdzokoto@gmail.com>
2022-06-30 21:13:25 -0700

    code a basic calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,47 +1,21 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys  import argv
-    from calculator_1 import *
-    count = len(argv)
 
-     if count != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-    num1 = int(argv[1])
-    num2 = int(argv[3])
-    op = argv[2]
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-    def not_found():
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
 
-    def add_():
-        total = add(num1, num2)
-        print("{:d} + {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    def sub_():
-        total = sub(num1, num2)
-        print("{:d} - {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    def mul_():
-        total = mul(num1, num2)
-        print("{:d} * {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    def div_():
-        total = div(num1, num2)
-        print("{:d} / {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    options = {
-        "+": add_,
-        "-": sub_,
-        "*": mul_,
-        "/": div_
-    }
-    options.get(op, not_found)()
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
 
 
