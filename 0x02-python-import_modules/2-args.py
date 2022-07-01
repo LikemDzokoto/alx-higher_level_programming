805fbde86b91a13e03a1e2955d58e69bd3d8b0a0
LikemDzokoto <likemdzokoto@gmail.com>
2022-06-30 20:29:38 -0700

    argvs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
+    total = len(sys.argv)
+    if total <= 1:
         print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
     else:
-        print("{} arguments:".format(count))
-    for i  in range(count):
-        print("{} : {}".format(i + 1, sys.argv[i + 1]))
+        if total == 2:
+            print("{:d} argument:".format(total - 1))
+        else:
+            print("{:d} arguments:".format(total - 1))
+        for i in range(1, total):
+            print("{:d}: {}".format(i, sys.argv[i]))
